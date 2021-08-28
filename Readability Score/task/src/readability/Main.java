62b76f142da770fe4cc60cb154ee4e0b40f89a99
Sergey Soroka <dragoon8370@gmail.com>
2021-08-29 01:41:28 +0300

    Stage 2/4 completed.

--- Readability Score/task/src/readability/Main.java
+++ Readability Score/task/src/readability/Main.java
@@ -6,6 +6,17 @@ public class Main {
     final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         String inputString = scanner.nextLine();
-        System.out.print(inputString.length() > 100? "HARD" : "EASY");
+        String[] sentences = inputString.split("[.!?]");
+
+        int numberOfWords = 0;
+        int numberOfSentences = 0;
+        for (String sentence :
+                sentences) {
+            numberOfSentences++;
+            numberOfWords += sentence.split("\\s+").length;
+        }
+
+        boolean isHardToRead = (double) (numberOfWords / numberOfSentences) > 10;
+        System.out.println(isHardToRead ? "HARD" : "EASY");
     }
 }
